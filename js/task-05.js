f7a0f45e5a0adc511331223d5cdf5969d90e24a8
Temur Esanov <temuresanov01@gmail.com>
2021-08-26 21:16:58 +0200

    Задача 5

---
+++ js/task-05.js
@@ -0,0 +1,11 @@
+let nameInput = document.getElementById("name-input");
+
+let nameOutput = document.getElementById("name-output");
+
+nameInput.oninput = function () {
+    if (nameInput.value === "") {
+       nameOutput.innerHTML = "незнакомец";
+    } else{
+    nameOutput.innerHTML = nameInput.value;
+    }
+}
