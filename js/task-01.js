e4a5b53fc05dfc1501afff4ef5f9abb39a3b6a11
Temur Esanov <temuresanov01@gmail.com>
2021-08-26 19:11:32 +0200

    правки 1 задачи

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,10 @@
 const categories = document.querySelectorAll("#categories li.item");
 console.log(`В списке ${categories.length} категории.`);
 
-
 const childCategories = array => {
-    array.forEach((element) => (`Категория:` + element.firstElementChild.textContent + `\nКоличество элементов:` + element.lastElementChild.children.length));
+    array.forEach((element) => console.log
+        (`Категория:` + element.firstElementChild.textContent +
+            `\nКоличество элементов:` + element.lastElementChild.children.length));
 }
-console.log (childCategories(categories));
+childCategories(categories);
 
