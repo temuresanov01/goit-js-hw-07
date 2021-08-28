23f8164cb1d8ae908f9c394ec4098ec85979ce72
Temur Esanov <temuresanov01@gmail.com>
2021-08-28 12:40:31 +0200

    6 задача
    2 метода через тернарный  ?  и через if else

--- js/task-06.js
+++ js/task-06.js
@@ -1,32 +1,29 @@
+             // метод 1
+
+// const inputRef = document.querySelector('#validation-input');
+// const SymbolsInputRef = Number(inputRef.getAttribute('data-length'));
+
+// const onInputBlur = () =>
+//     inputRef.value.length === SymbolsInputRef ? inputRef
+//         .className = 'valid' : inputRef.className = 'invalid';
+
+// inputRef.addEventListener('blur', onInputBlur);
+
+
+            //  метод 2 
 
 const inputRef = document.querySelector('#validation-input');
 const SymbolsInputRef = Number(inputRef.getAttribute('data-length'));
 
-// метод 1
+console.log(Number(SymbolsInputRef));
 
-const onInputBlur = () =>
-    inputRef.value.length === SymbolsInputRef ? inputRef
-        .className = 'valid' : inputRef.className = 'invalid';
+const onInputBlur = () => {
+    
+    if (inputRef.value.length === SymbolsInputRef) {
+        inputRef.className = 'valid'
+    } else {
+        inputRef.className = 'invalid'
+    }
+}
 
 inputRef.addEventListener('blur', onInputBlur);
-
-
-//  метод 2
-
-// const inputRef = document.querySelector('input#validation-input');
-// const SymbolsInputRef = validationInput.getAttribute('data-length');
-// // console.log(Number(SymbolsInputRef));
-
-// const onInputFocus = (event) => {
-//     const eventValue = event.currentTarget.value;
-
-//     if (eventValue.length === Number(SymbolsInputRef)) {
-//         inputRef.classList.add('valid');
-//         inputRef.classList.remove('invalid');
-//     } else {
-//         inputRef.classList.add('invalid');
-//         inputRef.classList.remove('valid');
-//     }
-// }
-
-// inputRef.addEventListener('focus', onInputFocus);
