b4767ba7765819084f35738a0ee68fba0f114f81
Temur Esanov <temuresanov01@gmail.com>
2021-08-28 11:00:49 +0200

    правки 8 задача

--- js/task-08.js
+++ js/task-08.js
@@ -6,18 +6,21 @@ const boxRef = document.querySelector('#boxes');
 renderBtnRef.addEventListener('click', () => createBoxes(inputRef.value));
 destroyBtnRef.addEventListener('click', destroyBoxes);
 
-function createBoxes(amount) {
+function createBoxes(value) {
     let newBoxes = [];
-    for (let i = 0; i < amount; i += 1) {
+    for (let i = 0; i < value; i += 1) {
         const newBox = document.createElement('div');
+        newBox.style.backgroundColor = `rgb(${getRandom(0, 255)}, ${getRandom(0, 255)}, ${getRandom(0, 255)})`;
         newBox.style.width = `${30 + i * 10}px`;
         newBox.style.height = `${30 + i * 10}px`;
-        newBox.style.backgroundColor =
-            `rgb(${getRandom(0, 255)}, ${getRandom(0, 255)}, ${getRandom(0, 255)})`;
         newBoxes.push(newBox);
     }
     boxes.append(...newBoxes);
+    
+    function getRandom(min, max){
+  return Math.ceil(Math.random() * (max - min) + min)
 }
+};
 
 function destroyBoxes() {
     boxes.innerHTML = '';
